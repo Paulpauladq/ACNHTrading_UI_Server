2cb022efc2838ba960e833896302982f5f225d14
Paulpauladq <paultzq@hotmail.com>
2020-08-11 06:16:29 -0700

    Add listing displaying for acnh trading user

--- src/content/profile/ProfileNavBar.jsx
+++ src/content/profile/ProfileNavBar.jsx
@@ -1,30 +1,49 @@
-import { Button, ButtonGroup } from 'react-bootstrap';
+import {
+  Button, ButtonGroup, Tabs, Tab,
+} from 'react-bootstrap';
 import React from 'react';
+import AcnherListing from '../listing/AcnherListing.jsx';
 
-function ProfileNavBar() {
+function ProfileNavBar(props) {
+  const { id } = props;
+
   return (
-    <ButtonGroup>
-      <Button>
-        <i className="fas fa-list" />
-        {' '}
-        Listings
-      </Button>
+    <React.Fragment>
+      <Tabs defaultActiveKey={1} id="uncontrolled-tab-example">
+        <Tab eventKey={1} title="Listings">
+          <AcnherListing id={id} />
+        </Tab>
+        <Tab eventKey={2} title="Offers">
+          Tab 2 content
+        </Tab>
+        <Tab eventKey={3} title="Wishlist">
+          Tab 3 content
+        </Tab>
+      </Tabs>
+      <ButtonGroup>
+        <Button>
+          <i className="fas fa-list" />
+          {' '}
+          Listings
+        </Button>
 
 
-      <Button>
-        <i className="fas fa-user-tag" />
-        {' '}
-        Offers
-      </Button>
+        <Button>
+          <i className="fas fa-user-tag" />
+          {' '}
+          Offers
+        </Button>
 
 
-      <Button>
-        <i className="fas fa-grin-hearts" />
-        {' '}
-        WishList
-      </Button>
+        <Button>
+          <i className="fas fa-grin-hearts" />
+          {' '}
+          WishList
+        </Button>
 
-    </ButtonGroup>
+      </ButtonGroup>
+    </React.Fragment>
+
   );
 }
 
